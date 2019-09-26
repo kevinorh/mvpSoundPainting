c3eaed2b4637b9d7d39a7b17cca95381d00d8b86
Kevin Ore <kevin.ore@cxmile.com>
2019-09-26 16:35:09 -0500

    avance mvp 1

--- BodyDetection/ViewController.swift
+++ BodyDetection/ViewController.swift
@@ -14,16 +14,23 @@ class ViewController: UIViewController, ARSessionDelegate {
 
     @IBOutlet var arView: ARView!
     @IBOutlet weak var messageLabel: MessageLabel!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var labely: UILabel!
+    @IBOutlet weak var labelz: UILabel!
+    @IBOutlet weak var labelSpeed: UILabel!
     
     // The 3D character to display.
     var character: BodyTrackedEntity?
-    let characterOffset: SIMD3<Float> = [-1.0, 0, 0] // Offset the character by one meter to the left
+    var characterOffset: SIMD3<Float> = [1, 0, 0] // Offset the character by one meter to the left
     let characterAnchor = AnchorEntity()
     
     // A tracked raycast which is used to place the character accurately
     // in the scene wherever the user taps.
     var placementRaycast: ARTrackedRaycast?
     var tapPlacementAnchor: AnchorEntity?
+    
+    var lastHandPosition: [Float] = [0,0,0]
+    var count = 0;
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -52,7 +59,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         }, receiveValue: { (character: Entity) in
             if let character = character as? BodyTrackedEntity {
                 // Scale the character to human size
-                character.scale = [1.0, 1.0, 1.0]
+                character.scale = [0.3, 0.3, 0.3]
                 self.character = character
                 cancellable?.cancel()
             } else {
@@ -64,7 +71,19 @@ class ViewController: UIViewController, ARSessionDelegate {
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         for anchor in anchors {
             guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
-            
+            var handPosition = simd_make_float3((bodyAnchor.skeleton.modelTransform(for: ARSkeleton.JointName.rightHand)?.columns.3)!);
+            label.text = "x: \(handPosition.x*100)";
+            labely.text = "y: \(handPosition.y*100)";
+            labelz.text = "z: \(handPosition.z*100)";
+            if(count == 30){
+            var speed = pow(handPosition.x - lastHandPosition[0],2)+pow(handPosition.y - lastHandPosition[1],2)+pow(handPosition.z - lastHandPosition[2],2);
+                //pow((handPosition.x - lastHandPosition[0]), 2) + pow( handPosition.x - lastHandPosition[1], 2) + pow(handPosition.x - lastHandPosition[2], 2)
+            labelSpeed.text = "speed: \(sqrtf(speed)*60)"
+            print(sqrtf(speed)*60)
+                count = 0
+            }
+            lastHandPosition = [handPosition.x , handPosition.y , handPosition.z]
+            characterOffset = handPosition;
             // Update the position of the character anchor's position.
             let bodyPosition = simd_make_float3(bodyAnchor.transform.columns.3)
             characterAnchor.position = bodyPosition + characterOffset
@@ -79,5 +98,6 @@ class ViewController: UIViewController, ARSessionDelegate {
                 characterAnchor.addChild(character)
             }
         }
+        count += 1
     }
 }
